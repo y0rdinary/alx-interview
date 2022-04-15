4c578a0352a9f836b80823b355572fbf01eda83b
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-15 17:54:23 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+def canUnlockAll (boxes):
+    unlocked = [0]
+    for boxId, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != boxId:
+                unlocked.append(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
