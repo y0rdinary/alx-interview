512ad2304193eb19fb537ab0af77f86caeb0426b
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-26 16:42:25 +0300

    operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H.
+Your text editor can execute only two operations
+in this file: Copy All and Paste. Given a number n,
+write a method that calculates the fewest
+number of operations needed to result in exactly n H characters
+in the file.
+"""
+
+
 def factorize(n):
     """ returns the sum of prime factors of n or n if n is prime"""
     initial = 2
