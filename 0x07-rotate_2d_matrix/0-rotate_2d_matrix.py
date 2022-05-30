d543a52d834b37fd3416966f1d3d2e4208b176db
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-05-30 08:56:13 +0000

    rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 
+A function to rotate n X n matrix 
+"""
+
+def rotate_2d_matrix(matrix):
+    rotate = []
+    for i in range(0, len(matrix)):
+        rotate.append([j[i] for j in reversed(matrix)])
+    matrix.clear()
+    matrix += rotate
