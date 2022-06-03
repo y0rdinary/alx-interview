949b3f58fd155b8d5dbaa20dad03cc309a319366
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-06-03 09:20:05 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+"""
+Given a pile of coins of different values,
+determine the fewest number of coins needed
+to meet a given amount total.
+"""
 
 
 def makeChange(coins, total):
-    count_amount = 0
-    count_coins = 0
-    if total <= 0:
-        return 0
-    else:
-        while count_amount < total:
-            if len(coins) == 0:
-                return -1
-            count_amount += max(coins)
-                if count_amount > total:
+        count_amount = 0
+        count_coins = 0
+        if total <= 0:
+           return 0
+        else:
+           while count_amount < total:
+                 if len(coins) == 0:
+                    return -1
+                 count_amount += max(coins)
+                 if count_amount > total:
                     count_amount -= max(coins)
                     coins.remove(max(coins))
-                else:
-                    count_coins =+ 1
+                 else:
+                    count_coins += 1
 
-        return coint_coins
+        return count_coins
