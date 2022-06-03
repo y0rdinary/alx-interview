800d595dd80b2b72daf4f69093fbf46c97d3bfcf
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-06-03 09:13:50 +0000

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    count_amount = 0
+    count_coins = 0
+    if total <= 0:
+        return 0
+    else:
+        while count_amount < total:
+            if len(coins) == 0:
+                return -1
+            count_amount += max(coins)
+                if count_amount > total:
+                    count_amount -= max(coins)
+                    coins.remove(max(coins))
+                else:
+                    count_coins =+ 1
+
+        return coint_coins
